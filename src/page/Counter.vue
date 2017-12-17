b39fd47ece677d74d3ca8b0d28dd5b9aa392e64d
franklion <franklion1227@gmail.com>
2017-12-18 00:46:34 +0800

    修改counter為簡單版本

--- src/page/Counter.vue
+++ src/page/Counter.vue
@@ -1,19 +1,9 @@
 <template>
   <div class="container">
-    <h2 style="color: red;">count: {{ count }}</h2>
-
-    <!-- 1. 增加 input 可以設定數值 model 綁定 num -->
-    Set Number: 
-    <input type="number" v-model.number="num" style="width: 50px;"><br/>
-    
-    <!-- 3. 雙向綁定 num 到按鈕上顯示-->
-    <!-- 傳 num 數值給 action -->
-    <button @click="actionIncrease(num)">+{{ num }}</button>
-    <button @click="actionDecrease(num)">-{{ num }}</button>
-
-    <!-- 4. 新增歸零按鈕 -->
-    <button @click="actionCountReset">歸零</button>
-  </div>
+    <h1>counter page</h1>
+    <p>{{ count }}</p>
+    <button @click="actionIncrease(5)">add</button>
+  </div>  
 </template>
 
 <script>
@@ -24,22 +14,15 @@ import { mapGetters, mapActions } from "vuex";
 
 export default {
   data() {
-    return {
-      // 2. model num 預設值為: 1
-      num: 1
-    };
+    return {};
   },
   computed: {
-    count: "getCount"
+    ...mapGetters({
+      count: "getCount"
+    })
   },
   methods: {
-    ...mapActions([
-      "actionIncrease",
-      "actionDecrease",
-
-      // 5. 引入歸零 action
-      "actionCountReset"
-    ])
+    ...mapActions(["actionIncrease"])
   }
 };
 </script>
--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,9 +1,3 @@
-const types = {
-  INCREASE: 'count/INCREASE',
-  DECREASE: 'count/DECREASE',
-  COUNT_RESET: 'count/COUNT_RESET'
-}
-
 const state = {
   count: 0
 }
@@ -12,48 +6,26 @@ const getters = {
   getCount: state => state.count
 }
 
-//非同步
 const actions = {
   actionIncrease({
     commit
-  }, num) {
-    commit(types.INCREASE, num);
-  },
-  actionDecrease({
-    commit
-  }, num) {
-    commit(types.DECREASE, num);
-  },
-  actionCountReset({
-    commit
-  }) {
-    commit(types.COUNT_RESET);
+  }, number ) {
+    // console.log(number)
+    commit('increase', number)
   }
 }
 
-// 同步
+
 const mutations = {
-  [types.INCREASE](state, num) {
-    state.count += num;
-    console.log('newINCREASE', num, 'state?', state.count);
-  },
-  [types.DECREASE](state, num) {
-    state.count -= num;
-    console.log('newDECREASE', num, 'state?', state.count);
-  },
-  [types.COUNT_RESET](state) {
-    state.count = 0;
-    console.log('newCOUNT_RESET - state?', state.count);
+  ['increase'] (state, number) {
+    state.count += number
   }
 }
 
-/*
-因為我們把 vuex 所有職權都寫在同一隻檔案，
-所以必須要 export 出去給最外層 index.js 組合使用
-*/
+
 export default {
-  state,
-  getters,
   actions,
-  mutations
+  mutations,
+  getters,
+  state,
 }
