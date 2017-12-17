09850c50118da9022091d14151e23b13fd970bb0
franklion <franklion1227@gmail.com>
2017-12-17 13:30:25 +0800

    counter 修改

--- src/page/Counter.vue
+++ src/page/Counter.vue
@@ -17,6 +17,9 @@
 </template>
 
 <script>
+// 引用 vuex
+// mapActions 在 computed 中使用，提取 action 函式的方法，使用函式名稱
+// mapGetters 在 methods 中使用，提取 getter 函式的方法，可以利用物件 key: value 方式取別名
 import { mapGetters, mapActions } from "vuex";
 
 export default {
@@ -26,9 +29,9 @@ export default {
       num: 1
     };
   },
-  computed: mapGetters({
+  computed: {
     count: "getCount"
-  }),
+  },
   methods: {
     ...mapActions([
       "actionIncrease",
