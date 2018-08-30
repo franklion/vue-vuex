d90c3e1280257262d75a84a084d0733bc8d724b3
franklion <franklion1227@gmail.com>
2018-08-30 18:47:41 +0800

    [ update 測試載入圖片 ]

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -1,8 +1,31 @@
 <template lang="pug">
   div.home-container
     h1 Welcome to Vue-vuex :)
-    img.home-page-cover(src="../assets/images/lion.svg")
+    img.home-page-cover(:src="cover")
+    //- img(src='../assets/images/lion.svg')
+    input#datepicker(type="text" v-model="date")
 </template>
+
+<script>
+// import icon from '../assets/images/lion.svg' // O
+// import icon from '@/assets/images/lion.svg' // O
+
+  export default {
+    data() {
+      return {
+        date: 'hello',
+        // cover: '../assets/images/lion.svg' // X
+        // cover: '/static/images/lion.svg' // O
+        // cover: icon
+        cover: require('@/assets/images/lion.svg') // O
+      }
+    },
+    mounted() {
+        $('.home-page-cover').fadeOut(1500)
+        $('#datepicker').datepicker();
+    }
+  }
+</script>
 
 
 <style lang="sass">
